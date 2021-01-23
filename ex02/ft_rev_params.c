c38c0c149299c94b5486e3f9daaf244064031968
katrin19 <58865678+katrin19@users.noreply.github.com>
2021-01-23 19:38:12 +0400

    c06

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpetrosy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 17:40:19 by kpetrosy          #+#    #+#             */
+/*   Updated: 2020/12/09 18:03:43 by kpetrosy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i--;
+	}
+	return (0);
+}
